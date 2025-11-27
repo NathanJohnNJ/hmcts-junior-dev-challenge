a35b14c812930deb88de7471fd6ad91f26ad763c
Nathan John <nj@njtd.xyz>
2025-11-27 22:49:56 +0000

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Home from './pages/home';
-import Tasks from './pages/tasks';
 import Root from './pages/root';
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={ <Home /> } />
-      <Route path="/tasks" element={ <Tasks />} />
     </Route>
   ))
 
